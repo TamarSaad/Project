f52a57a4aab4f87b12f32cb3b3df001bfd4ed843
TamarSaad <72976260+TamarSaad@users.noreply.github.com>
2022-09-20 08:13:52 +0300

    Delete אנליזות/16S/scripts directory

--- אנליזות/16S/scripts/classifying/sklearn-gg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-INPUT_DIR="$1"
-OUTPUT_DIR="$2"
-#sklearn classifying by greengenes
-qiime feature-classifier classify-sklearn --i-reads ${INPUT_DIR}dada2_rep-seqs.qza --i-classifier ~/Documents/gg-13-8-99-nb-classifier.qza --o-classification ${OUTPUT_DIR}greengenes/qza/classified.qza --verbose
-#visualization
-qiime metadata tabulate --m-input-file ${OUTPUT_DIR}greengenes/qza/classified.qza --o-visualization ${OUTPUT_DIR}greengenes/vis/taxonomy.qzv
--- אנליזות/16S/scripts/classifying/sklearn-silva.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-INPUT_DIR="$1"
-OUTPUT_DIR="$2"
-#sklearn classifying by silva
-qiime feature-classifier classify-sklearn --i-reads ${INPUT_DIR}dada2_rep-seqs.qza --i-classifier ~/Documents/silva-138-99-nb-classifier.qza --o-classification ${OUTPUT_DIR}silva/qza/classified.qza --verbose
-#visualization
-qiime metadata tabulate --m-input-file ${OUTPUT_DIR}silva/qza/classified.qza --o-visualization ${OUTPUT_DIR}silva/vis/taxonomy.qzv
