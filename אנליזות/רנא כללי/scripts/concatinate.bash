833cbaa0143b96a027dbbe89e2b8662105b460f6
TamarSaad <72976260+TamarSaad@users.noreply.github.com>
2022-09-20 08:16:31 +0300

    Delete אנליזות/רנא כללי/scripts directory

--- אנליזות/רנא כללי/scripts/concatinate.bash
+++
@@ -1,5 +0,0 @@
-for i in $(ls -R *.fastq.*) do
-	echo $(ls -R ${i%_L00*});
-	echo $(ls -R ${i%_L00*}*.fastq.gz);
-	cat $(ls -R ${i%_L00*}*.fastq.gz)> ../fastq_concatinated/${i%_L00*}.fastq.gz;
-done
--- אנליזות/רנא כללי/scripts/versions.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#print the version of each program
-declare -a StringArray=("fastqc" "multiqc" "STAR" "samtools") #"htseq-count" "trimmomatic")
-echo "check manually htseq-count version by printing "htseq-count --help"">versions.txt
-echo "########################################################">>versions.txt
-for program in ${StringArray[@]}; do
-	echo ${program}:
-  $program --version;
-done &>>versions.txt&
